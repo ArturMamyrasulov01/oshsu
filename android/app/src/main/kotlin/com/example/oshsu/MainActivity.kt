3f77b532ec41f80df5a8aa0af695008cceeeb22d
Artur Mamyrasulov <arturmamyrasulov@gmail.com>
2022-12-21 10:58:02 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/oshsu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oshsu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WebviewScaffold(
+        url: ('https://mail.oshsu.kg/mail/'),
+      ),
+    );
+  }
+}
